c0a3e7ef47363ade924dbbf04d6cc686602e51e7
yjshin.cloud <yjshin.cloud@gmail.com>
2025-09-04 10:08:19 +0900

    Initial commit

---
+++ src/test/java/com/example/springrefresh/SpringrefreshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springrefresh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringrefreshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
